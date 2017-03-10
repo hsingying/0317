c31ef9e4e6e34dbed600b0ea4dc0588847ca0f6b
HSINGYING_YO <HSINGYING_YO@ASUS>
2017-03-10 16:06:12 +0800

    Add project files.

---
+++ 0310/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_0310.Startup))]
+namespace _0310
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
