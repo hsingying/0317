a89c901a1c80dd4c7b5935738b0299f76b23070f
hsingying <1103137212@gm.kuas.edu.tw>
2017-03-18 01:18:09 +0800

    2222

--- 0310/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_0310.MvcApplication" Language="C#" %>
---
+++ 0317/Areas/Order/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0317.Areas.Order.Controllers
+{
+    public class OrderController : Controller
+    { // GET: Order/Order
+        /// <summary>
+        /// 訂單管理系統
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            Models.OrderService orderservice = new Models.OrderService();
+            var order = orderservice.GetOrderById("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單的畫面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單存檔的action
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            orderService.InsertOrder(order);
+            return View("Index");
+        }
+    }
+}
